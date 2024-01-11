e63b40ec88cff913da3cc10130a0f0b5c65624c4
oN0V41S <rafaelaugustonnovais@gmail.com>
2024-01-11 20:51:27 -0300

    função matricular agora insere o usuario na materia

--- desafio.kt
+++ desafio.kt
@@ -11,11 +11,13 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.append(usuario)
     }
 }
 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlin = ConteudoEducacional("Curso Kotlin")
+    println(kotlin)
 }
